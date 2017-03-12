9bce9b4b744b56a61817d6e69f41bf0d451597ab
Dhanraj99 <dhanraj.m.pardeshi@gmail.com>
2017-03-12 17:26:14 +0530

    [imad-console] Updates ui/main.js

---
+++ ui/main.js
@@ -0,0 +1,12 @@
+var button = document.getElementById('counter');
+countre = 0;
+button.onclick = function(){
+    
+    //make a req 
+    //Capture res
+    //Store in var
+    //Render var
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+};
