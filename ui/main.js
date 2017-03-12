b59db89edf4ee4ec370d9db854698d55f89a6fdf
Dhanraj99 <dhanraj.m.pardeshi@gmail.com>
2017-03-12 19:25:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,5 +20,11 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
-    
+    var names = ['A','b'];
+    var list = '';
+    for(var i=0;i<names.length;i++){
+        list = '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
 };
