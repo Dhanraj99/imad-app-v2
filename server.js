c578180b9c1341ef528bcaee912490201e35cb88
Dhanraj99 <dhanraj.m.pardeshi@gmail.com>
2017-03-12 20:05:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -55,6 +55,13 @@ app.get('/counter', function(req,res) {
    res.send(counter.toString());
 });
 
+var names = [];
+app.get('/submit-name/:name',function(req,res){
+    var name = req.params.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
+
 app.get('/:articleName',function (req,res){
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
@@ -70,13 +77,6 @@ app.get('/ui/main.js', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-var names = [];
-app.get('/submit-name/:name',function(req,res){
-    var name = req.params.name;
-    names.push(name);
-    res.send(JSON.stringify(names));
 });
 
 function createTemplate(data){
