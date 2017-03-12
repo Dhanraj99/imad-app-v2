d92c9aeb1fbe2db5d25be8276af3b4a50ddfd827
Dhanraj99 <dhanraj.m.pardeshi@gmail.com>
2017-03-12 17:08:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,11 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
+
 var articles={
     'article-one' : {
     title:'Article One | dp',
@@ -39,11 +44,6 @@ var articles={
             </p>`
     }
 };
-
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
 
 function createTemplate(data){
     var title = data.title;
